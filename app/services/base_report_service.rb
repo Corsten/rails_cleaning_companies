faedb7d2e0b719965efd62cf5284a01005eb2c91
Anastasia Moshina <anastasiya.moshina@rt.ru>
2021-03-18 13:29:53 +0400

    CLEAN-5. Rubocop at works

--- app/services/base_report_service.rb
+++ app/services/base_report_service.rb
@@ -1,5 +1,4 @@
 class BaseReportService
-
   def export_to_xlsx(data, list_name)
     package = Axlsx::Package.new
     workbook = package.workbook
@@ -9,7 +8,7 @@ class BaseReportService
 
     workbook.add_worksheet(name: list_name) do |sheet|
       sheet.add_row(report_table_header, style: workbook_styles[:header])
-      @data.each { |client| sheet.add_row(report_table_info(client), style: workbook_styles[:default_cell]) }
+      data.each { |client| sheet.add_row(report_table_info(client), style: workbook_styles[:default_cell]) }
     end
 
     package.to_stream.read
