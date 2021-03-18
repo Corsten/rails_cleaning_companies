553059b04d602ed5ffbbaad1ecdd1ef218b1b9fe
Anastasia Moshina <anastasiya.moshina@rt.ru>
2021-03-18 12:57:40 +0400

    CLEAN-5. Rubocop at works

--- app/services/clients_report_service.rb
+++ app/services/clients_report_service.rb
@@ -6,9 +6,9 @@ class ClientsReportService < BaseReportService
   private
 
   def report_table_header
-    [translate('client.', 'id'), translate('client.','name'),
-     translate('client.','surname'), translate('client.','email'),
-     translate('client.','phone_number')]
+    [translate('client.', 'id'), translate('client.', 'name'),
+     translate('client.', 'surname'), translate('client.', 'email'),
+     translate('client.', 'phone_number')]
   end
 
   def report_table_info(client)
