3997bd74a4bc33d82dd2bf3d10a845ad97071452
Anastasia Moshina <anastasiya.moshina@rt.ru>
2021-03-24 17:03:07 +0400

    CLEAN-6. Report.state is enumerize

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,9 @@
 class Report < ApplicationRecord
   include StateMashines::ReportStateMashine
+  extend Enumerize
   paginates_per 15
 
   belongs_to :admin
   validates :state, :file_type, presence: true
+  enumerize :state, in: %i[new in_process done], default: :new
 end
